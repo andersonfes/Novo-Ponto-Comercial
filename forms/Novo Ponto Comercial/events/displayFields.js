03272626afe0c808e4f1cac68556a0e86e8a5cd0
Anderson Fernandes <andersonfez.afds@gmail.com>
2024-11-01 07:22:58 -0300

    feat: Nova funcionalidade do botão 'Inserir Responsável'

--- forms/Novo Ponto Comercial/events/displayFields.js
+++ forms/Novo Ponto Comercial/events/displayFields.js
@@ -4,28 +4,30 @@ function displayFields(form, customHTML) {
     var numAtividade = getValue("WKNumState");
     var usuarioAtual = getValue("WKUser");
 
-    customHTML.append(`<script>\n`);
-    customHTML.append(`\n\tvar MBPM = MBPM || {};\n`);
-    customHTML.append(`\tMBPM.wkuser = '${usuarioAtual}';\n`);
+    customHTML.append('<script>\n');
+    customHTML.append('\n\tvar MBPM = MBPM || {};\n');
+    customHTML.append("\tMBPM.wkuser = '" + usuarioAtual + "';\n");
 
     if (form.getFormMode() != 'ADD' && form.getFormMode() != 'MOD') {
         form.setHidePrintLink(false);
-        customHTML.append("\tthis.atividades.reloadProcessDetails();\n")
+        customHTML.append("\tpontoComercial.atividades.reloadProcessDetails();\n")
     } else {
         form.setHidePrintLink(true);
         if (numAtividade == '0' || numAtividade == INICIO) {
-            customHTML.append("\tthis.atividades.atInicial();\n");
+            customHTML.append("\tpontoComercial.atividades.atInicial();\n");
         }
         if (numAtividade == NEGOCIOS) {
-            customHTML.append("\tthis.atividades.atAproNegocio();\n");
+            customHTML.append("\tpontoComercial.atividades.atAproNegocio();\n");
         }
         if (numAtividade == SUPERINTENDENCIA) {
-            customHTML.append("\tthis.atividades.atAproSuperintedencia();\n");
+            customHTML.append("\tpontoComercial.atividades.atAproSuperintedencia();\n");
         }
         if (numAtividade == REVISAR) {
-            customHTML.append("\tthis.atividades.atRevisar();\n");
+            customHTML.append("\tpontoComercial.atividades.atRevisar();\n");
         }
     }
+
+    customHTML.append("\n</script>");
 
     log.info("-> Fim | Novo Ponto Comercial | displayFields");
 }
--- forms/Novo Ponto Comercial/script.js
+++ forms/Novo Ponto Comercial/script.js
@@ -2,7 +2,8 @@ class PontoComercial {
     atividades = {
 
         atInicial: () => {
-
+            pontoComercial.init.initData()
+            pontoComercial.init.initMascaras()
         },
 
         atAproNegocio: () => {
@@ -15,6 +16,31 @@ class PontoComercial {
 
         atRevisar: () => {
 
+        },
+
+        reloadProcessDetails: function () {
+            pontoComercial.init.initLayout();
+        }
+    };
+
+    init = {
+        initLayout: function () {
+            $('.box-label').attr('style', 'padding: 15px 15px 0 15px; margin: 30px 0 15px 0; border: 1px solid #ddd;');
+            $('.subtitle-02').attr('style', 'margin: -26px 0 10px 0; background-color: #fff; display: inline-block; font-weight: bold; text-transform: uppercase;');
+        },
+
+        initMascaras: () => {
+            $('#telefone').mask('(00) 00000-0000');
+            $('#docIdentidade').mask('00.000.000-0');
+            $('#cep').mask('00000-000');
+            $('#numero').mask('0000');
+        },
+
+        initData: () => {
+            var mySimpleCalendar = FLUIGC.calendar('#dataNascimento', {
+                language: 'pt-br',
+            }
+            ).setMaxDate(new Date());
         }
     };
 
@@ -23,7 +49,23 @@ class PontoComercial {
     };
 
     events = {
+        onClickDelete: (thisDiv) => {
+            var index = parseInt($(thisDiv).closest('tr')[0].id.replace(/[^0-9\.]/g, ''), 10);
+            FLUIGC.message.confirm({
+                message: 'Tem certeza que deseja remover?',
+                title: 'Remover Linha',
+                labelYes: 'Sim',
+                labelNo: 'Não'
+            }, function (result, el, ev) {
+                if (result) {
+                    $('#dpTbPlanTr___' + index).remove();
+                }
+            })
+        },
 
+        onclickAdd: () => {
+            var index = wdkAddChild('dpTbPlan');
+        },
     };
 
     dataset = {
